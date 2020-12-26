91e5bebd7faf4b4ce4afcfca8840604c2695049d
panzehir19 <github@omerustun.com.tr>
2020-12-26 17:18:11 +0300

    Update germain_asal.c

--- germain_asal.c
+++ germain_asal.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 	
     int a,b;
     int i;
-    
+    int ger_say = 0;
 	printf("Sinirlari girin:\n");
 	
 	scanf("%d %d",&a,&b);
 	
     printf("\n");
 	
-	for (i=a; i<=b; i++){
+	for (i=a; i<=b; i++)
+	{
 		
-        if (!(i == 0 || i == 1)) {
+        if (!(i == 0 || i == 1)) 
+		{
         	
 			int asal_sayac = 0;
 	        int x;
 			
-			for(x=2; x < (i); x++){
+			for(x=2; x < (i); x++)
+			{
 	
 	            if(i % x == 0)
 	                asal_sayac += 1;
 	
 	        }
-	        if (asal_sayac == 0) {
-	        	
+	        if (asal_sayac == 0) 
+			{
 	            int gerasal_sayac = 0;
 				int y;
 				
@@ -35,20 +37,20 @@ int main() {
 	                if ((2*i + 1) % y == 0)
 	                    gerasal_sayac += 1;
 				}
-	            if (gerasal_sayac == 0){
-				
+	            
+				if (gerasal_sayac == 0)		{
 	                printf("%d-",i);
-	                
+	                ger_say += 1;
 					}
-	                
-	            else {(printf("Belirtilen aralikta Germain asali bulunmamaktadir."));
-					
-					}		
+	
 				}
+				
 	    	}
 	    	
 	    }
-	    
+	    if (ger_say == 0)
+	    printf("Bu aralikta Germain asali bulunmamaktadir.");
+	    return 0;
     }
     
 
